de3f868e769c5dc993b64b00a09723edb87a3669
tunajp@gmail.com <tunajp@gmail.com@c869dabd-704f-0410-af2d-57d18ae99ed4>
2008-08-30 08:03:30 +0000

    複数行チャットのアスキーアート登録

--- 2ndviewer/AAForm.cs
+++ 2ndviewer/AAForm.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -11,28 +11,61 @@ namespace _2ndviewer
 {
     public partial class AAForm : Form
     {
+        private String[] dirs_;
         public AAForm()
         {
             InitializeComponent();
-            this.listBox1.Items.Add("スンマセン");
+            ReReadDirectory();
+        }
+
+        private void ReReadDirectory()
+        {
+            try
+            {
+                dirs_ = System.IO.Directory.GetFiles("asciiarts");
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.ToString(), "FileSystem Error", MessageBoxButtons.OK);
+                return;
+            }
+            this.listBox1.Items.Clear();
+            for (int i = 0; i < dirs_.Length; i++)
+            {
+                this.listBox1.Items.Add(dirs_[i].Substring(10, dirs_[i].Length - 4 - 10));
+            }
         }
 
         private void select_button_Click(object sender, EventArgs e)
         {
-            if (listBox1.SelectedIndex == 0) {
-                string text =
-                    "　　　　　　　 ∧＿∧　 ／￣￣￣￣￣￣￣\r\n" +
-                    "　　　　　　　（；´Д｀）＜　スンマセン、直ぐに片付けます\r\n" +
-                    "　　-=≡ 　/　　　 ヽ　 ＼＿＿＿＿＿＿＿\r\n" +
-                    "　　　　　 /|　|　　 |. |\r\n" +
-                    "　-=≡　/.　＼ヽ／＼＼_\r\n" +
-                    "　　　　/　　　 ヽ⌒)==ヽ_）=　∧＿∧\r\n" +
-                    "-= 　 /　/⌒＼.＼　||　　|| 　(´･ω･`)\r\n" +
-                    "　　／ ／　　　 >　） || 　 ||　( つ旦O\r\n" +
-                    "　/　/ 　　　　/ ／＿||＿ ||　と＿)＿) ＿.\r\n" +
-                    "　し'　　　　　（＿つ￣(_））￣ (.)）￣ (_））￣(.)）\r\n";
-                this.textBox1.Text = text;
+            string text = System.IO.File.ReadAllText(dirs_[listBox1.SelectedIndex], System.Text.Encoding.GetEncoding("UTF-8"));
+            title_textBox.Text = dirs_[listBox1.SelectedIndex].Substring(10, dirs_[listBox1.SelectedIndex].Length - 4 - 10);
+            this.textBox1.Text = text;
+        }
+
+        private void add_button_Click(object sender, EventArgs e)
+        {
+            if (title_textBox.Text.Length <= 0) {
+                MessageBox.Show(StringResource.titleIsNull, "Information", MessageBoxButtons.OK);
+                return;
             }
+            try
+            {
+                System.IO.File.WriteAllText("asciiarts\\" + title_textBox.Text + ".txt", textBox1.Text, System.Text.Encoding.GetEncoding("UTF-8"));
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString(), "FileSystem Error", MessageBoxButtons.OK);
+                return;
+            }
+            this.listBox1.Items.Add(title_textBox.Text);
+            ReReadDirectory();
+        }
+
+        private void clear_button_Click(object sender, EventArgs e)
+        {
+            this.title_textBox.Text = "";
+            this.textBox1.Text = "";
         }
     }
 }
