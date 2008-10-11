8bbf6864e0ae3419661711de0450abb550e86c08
tunajp@gmail.com <tunajp@gmail.com@c869dabd-704f-0410-af2d-57d18ae99ed4>
2008-10-11 14:37:48 +0000

    新規アカウント作成リンク

--- 2ndviewer/LoginForm.Designer.cs
+++ 2ndviewer/LoginForm.Designer.cs
@@ -43,101 +43,169 @@
             this.password_checkBox = new System.Windows.Forms.CheckBox();
             this.uri_checkBox = new System.Windows.Forms.CheckBox();
             this.uri_comboBox = new System.Windows.Forms.ComboBox();
+            this.AcountLinkLabel = new System.Windows.Forms.LinkLabel();
             this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
             // groupBox1
             // 
+            this.groupBox1.AccessibleDescription = null;
+            this.groupBox1.AccessibleName = null;
+            resources.ApplyResources(this.groupBox1, "groupBox1");
+            this.groupBox1.BackgroundImage = null;
             this.groupBox1.Controls.Add(this.nameloc_maskedTextBox);
             this.groupBox1.Controls.Add(this.nameloc_radioButton);
             this.groupBox1.Controls.Add(this.lastloc_radioButton);
-            resources.ApplyResources(this.groupBox1, "groupBox1");
+            this.groupBox1.Font = null;
             this.groupBox1.Name = "groupBox1";
             this.groupBox1.TabStop = false;
             // 
             // nameloc_maskedTextBox
             // 
+            this.nameloc_maskedTextBox.AccessibleDescription = null;
+            this.nameloc_maskedTextBox.AccessibleName = null;
             resources.ApplyResources(this.nameloc_maskedTextBox, "nameloc_maskedTextBox");
+            this.nameloc_maskedTextBox.BackgroundImage = null;
+            this.nameloc_maskedTextBox.Font = null;
             this.nameloc_maskedTextBox.Name = "nameloc_maskedTextBox";
             // 
             // nameloc_radioButton
             // 
+            this.nameloc_radioButton.AccessibleDescription = null;
+            this.nameloc_radioButton.AccessibleName = null;
             resources.ApplyResources(this.nameloc_radioButton, "nameloc_radioButton");
+            this.nameloc_radioButton.BackgroundImage = null;
+            this.nameloc_radioButton.Font = null;
             this.nameloc_radioButton.Name = "nameloc_radioButton";
             this.nameloc_radioButton.TabStop = true;
             this.nameloc_radioButton.UseVisualStyleBackColor = true;
             // 
             // lastloc_radioButton
             // 
+            this.lastloc_radioButton.AccessibleDescription = null;
+            this.lastloc_radioButton.AccessibleName = null;
             resources.ApplyResources(this.lastloc_radioButton, "lastloc_radioButton");
+            this.lastloc_radioButton.BackgroundImage = null;
             this.lastloc_radioButton.Checked = true;
+            this.lastloc_radioButton.Font = null;
             this.lastloc_radioButton.Name = "lastloc_radioButton";
             this.lastloc_radioButton.TabStop = true;
             this.lastloc_radioButton.UseVisualStyleBackColor = true;
             // 
             // label1
             // 
+            this.label1.AccessibleDescription = null;
+            this.label1.AccessibleName = null;
             resources.ApplyResources(this.label1, "label1");
+            this.label1.Font = null;
             this.label1.Name = "label1";
             // 
             // label2
             // 
+            this.label2.AccessibleDescription = null;
+            this.label2.AccessibleName = null;
             resources.ApplyResources(this.label2, "label2");
+            this.label2.Font = null;
             this.label2.Name = "label2";
             // 
             // label3
             // 
+            this.label3.AccessibleDescription = null;
+            this.label3.AccessibleName = null;
             resources.ApplyResources(this.label3, "label3");
+            this.label3.Font = null;
             this.label3.Name = "label3";
             // 
             // firstName_textBox
             // 
+            this.firstName_textBox.AccessibleDescription = null;
+            this.firstName_textBox.AccessibleName = null;
             resources.ApplyResources(this.firstName_textBox, "firstName_textBox");
+            this.firstName_textBox.BackgroundImage = null;
+            this.firstName_textBox.Font = null;
             this.firstName_textBox.Name = "firstName_textBox";
             // 
             // lastName_textBox
             // 
+            this.lastName_textBox.AccessibleDescription = null;
+            this.lastName_textBox.AccessibleName = null;
             resources.ApplyResources(this.lastName_textBox, "lastName_textBox");
+            this.lastName_textBox.BackgroundImage = null;
+            this.lastName_textBox.Font = null;
             this.lastName_textBox.Name = "lastName_textBox";
             // 
             // password_textBox
             // 
+            this.password_textBox.AccessibleDescription = null;
+            this.password_textBox.AccessibleName = null;
             resources.ApplyResources(this.password_textBox, "password_textBox");
+            this.password_textBox.BackgroundImage = null;
+            this.password_textBox.Font = null;
             this.password_textBox.Name = "password_textBox";
             // 
             // login_button
             // 
+            this.login_button.AccessibleDescription = null;
+            this.login_button.AccessibleName = null;
             resources.ApplyResources(this.login_button, "login_button");
+            this.login_button.BackgroundImage = null;
+            this.login_button.Font = null;
             this.login_button.Name = "login_button";
             this.login_button.UseVisualStyleBackColor = true;
             this.login_button.Click += new System.EventHandler(this.login_button_Click);
             // 
             // password_checkBox
             // 
+            this.password_checkBox.AccessibleDescription = null;
+            this.password_checkBox.AccessibleName = null;
             resources.ApplyResources(this.password_checkBox, "password_checkBox");
+            this.password_checkBox.BackgroundImage = null;
+            this.password_checkBox.Font = null;
             this.password_checkBox.Name = "password_checkBox";
             this.password_checkBox.UseVisualStyleBackColor = true;
             // 
             // uri_checkBox
             // 
+            this.uri_checkBox.AccessibleDescription = null;
+            this.uri_checkBox.AccessibleName = null;
             resources.ApplyResources(this.uri_checkBox, "uri_checkBox");
+            this.uri_checkBox.BackgroundImage = null;
+            this.uri_checkBox.Font = null;
             this.uri_checkBox.Name = "uri_checkBox";
             this.uri_checkBox.UseVisualStyleBackColor = true;
             // 
             // uri_comboBox
             // 
+            this.uri_comboBox.AccessibleDescription = null;
+            this.uri_comboBox.AccessibleName = null;
+            resources.ApplyResources(this.uri_comboBox, "uri_comboBox");
+            this.uri_comboBox.BackgroundImage = null;
+            this.uri_comboBox.Font = null;
             this.uri_comboBox.FormattingEnabled = true;
             this.uri_comboBox.Items.AddRange(new object[] {
             resources.GetString("uri_comboBox.Items"),
             resources.GetString("uri_comboBox.Items1"),
             resources.GetString("uri_comboBox.Items2")});
-            resources.ApplyResources(this.uri_comboBox, "uri_comboBox");
             this.uri_comboBox.Name = "uri_comboBox";
+            // 
+            // AcountLinkLabel
+            // 
+            this.AcountLinkLabel.AccessibleDescription = null;
+            this.AcountLinkLabel.AccessibleName = null;
+            resources.ApplyResources(this.AcountLinkLabel, "AcountLinkLabel");
+            this.AcountLinkLabel.Font = null;
+            this.AcountLinkLabel.Name = "AcountLinkLabel";
+            this.AcountLinkLabel.TabStop = true;
+            this.AcountLinkLabel.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.AcountLinkLabel_LinkClicked);
             // 
             // LoginForm
             // 
+            this.AccessibleDescription = null;
+            this.AccessibleName = null;
             resources.ApplyResources(this, "$this");
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = null;
+            this.Controls.Add(this.AcountLinkLabel);
             this.Controls.Add(this.uri_comboBox);
             this.Controls.Add(this.uri_checkBox);
             this.Controls.Add(this.password_checkBox);
@@ -149,7 +217,9 @@
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.groupBox1);
+            this.Font = null;
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Icon = null;
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "LoginForm";
@@ -177,5 +247,6 @@
         private System.Windows.Forms.CheckBox password_checkBox;
         private System.Windows.Forms.CheckBox uri_checkBox;
         private System.Windows.Forms.ComboBox uri_comboBox;
+        private System.Windows.Forms.LinkLabel AcountLinkLabel;
     }
 }
